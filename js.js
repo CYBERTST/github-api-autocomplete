701c9378575714f6c35f5806fea43dfa01d29936
Roman Savenkov <cybersportsmen1@mail.ru>
2026-04-04 16:24:10 +0400

    correct code sequence

--- js.js
+++ js.js
@@ -1,44 +1,3 @@
-const URL = "https://api.github.com/search/repositories";
-
-const searchWrapper = document.querySelector(".search-input");
-const inputBox = searchWrapper.querySelector("#repositoryInput");
-const suggestionBox = searchWrapper.querySelector(".autocom-box");
-const suggestionList = document.querySelector(".repository");
-
-inputBox.addEventListener("keyup", debounce(loadRepository, 500));
-
-suggestionBox.addEventListener("click", function (event) {
-  const li = event.target;
-  const repoElement = createElementHepler("li", "repository__item");
-  suggestionList.append(repoElement);
-
-  const textColumn = createElementHepler("div", "text-container");
-  const textLineName = createElementHepler("div", "text-line-name");
-  textLineName.textContent = "Name: " + li.textContent;
-  const textLineOwner = createElementHepler("div", "text-line-owner");
-  textLineOwner.textContent = "Owner: " + li.dataset.owner;
-  const textLineStars = createElementHepler("div", "text-line-stars");
-  textLineStars.textContent = "Stars: " + li.dataset.stars;
-  textColumn.append(textLineName);
-  textColumn.append(textLineOwner);
-  textColumn.append(textLineStars);
-
-  repoElement.append(textColumn);
-
-  const buttonClose = createElementHepler("button", "close-icon");
-  repoElement.append(buttonClose);
-  inputBox.value = "";
-  suggestionBox.replaceChildren();
-});
-
-suggestionList.addEventListener("click", function (event) {
-  const btn = event.target.closest(".close-icon");
-  if (btn) {
-    const card = btn.closest(".repository__item");
-    card.remove();
-  }
-});
-
 function loadRepository() {
   if (inputBox.value) {
     suggestionBox.replaceChildren();
@@ -96,3 +55,44 @@ function debounce(func, wait, immediate) {
     if (callNow) func.apply(context, args);
   };
 }
+
+const URL = "https://api.github.com/search/repositories";
+
+const searchWrapper = document.querySelector(".search-input");
+const inputBox = searchWrapper.querySelector("#repositoryInput");
+const suggestionBox = searchWrapper.querySelector(".autocom-box");
+const suggestionList = document.querySelector(".repository");
+
+inputBox.addEventListener("keyup", debounce(loadRepository, 500));
+
+suggestionBox.addEventListener("click", function (event) {
+  const li = event.target;
+  const repoElement = createElementHepler("li", "repository__item");
+  suggestionList.append(repoElement);
+
+  const textColumn = createElementHepler("div", "text-container");
+  const textLineName = createElementHepler("div", "text-line-name");
+  textLineName.textContent = "Name: " + li.textContent;
+  const textLineOwner = createElementHepler("div", "text-line-owner");
+  textLineOwner.textContent = "Owner: " + li.dataset.owner;
+  const textLineStars = createElementHepler("div", "text-line-stars");
+  textLineStars.textContent = "Stars: " + li.dataset.stars;
+  textColumn.append(textLineName);
+  textColumn.append(textLineOwner);
+  textColumn.append(textLineStars);
+
+  repoElement.append(textColumn);
+
+  const buttonClose = createElementHepler("button", "close-icon");
+  repoElement.append(buttonClose);
+  inputBox.value = "";
+  suggestionBox.replaceChildren();
+});
+
+suggestionList.addEventListener("click", function (event) {
+  const btn = event.target.closest(".close-icon");
+  if (btn) {
+    const card = btn.closest(".repository__item");
+    card.remove();
+  }
+});
